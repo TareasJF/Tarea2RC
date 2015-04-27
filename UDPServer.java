ff64a4a48b0b0f21d82a403c2ab476a1ce92f04f
JoseC <n3ggro@gmail.com>
2015-04-27 18:42:28 -0300

    while print

--- UDPServer.java
+++ UDPServer.java
@@ -139,7 +139,7 @@ class UDPServer implements Server {
     DatagramSocket dsoc=new DatagramSocket(dataP);
     int i=0;
     while(f.available()!=0) {
-      System.out.println(" whiiiiiile "+Integer.parseInt(i));
+      System.out.println(" whiiiiiile "+Integer.toString(i));
       b[i]=(byte)f.read();
       i++;
     }                     
